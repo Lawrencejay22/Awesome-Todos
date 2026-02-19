b551573344e375db7255f35fcb9fce3c627a6eee
Lawrence Jay Gabionza <gabionzalawrencejay@gmail.com>
2026-02-19 23:07:04 +0800

    Initial commit

---
+++ server/routes.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const { getConnectedClient } = require("./database");
+const { ObjectId } = require("mongodb");
+
+
+const getCollection = () => {
+  const client = getConnectedClient();
+  const collection = client.db("todosdb").collection("todos");
+  return collection;
+}
+//==========DATABASE TABLE(todos)====================
+
+// GET /todos
+router.get("/todos", async (req, res) => {
+  const collection = getCollection();
+  const todos = await collection.find({}).toArray();
+
+  res.status(200).json({ todos });
+});
+
+// POST /todos
+router.post("/todos", async (req, res) => {
+  try {
+    const collection = getCollection();
+    let { todo } = req.body;
+
+    if (!todo) {
+      return res.status(400).json({ mssg: "error no todo found" });
+    }
+
+
+
+    const newTodo = await collection.insertOne({ todo, status: false });
+
+    res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
+  } catch (error) {
+    console.error("POST /todos error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// DELETE /todos/:id
+router.delete("/todos/:id", async (req, res) => {
+  const collection = getCollection();
+  const _id = new ObjectId(req.params.id);
+
+  const deleteTodo = await collection.deleteOne({ _id });
+
+  res.status(200).json(deleteTodo);
+});
+
+// PUT /todos/:id
+router.put("/todos/:id", async (req, res) => {
+  const collection = getCollection();
+  const _id = new ObjectId(req.params.id);
+  const { status } = req.body;
+
+  if (typeof status !== "boolean") {
+    return res.status(400).json({ mssg: "invalid status" });
+  }
+
+  const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } });
+
+  res.status(200).json(updatedTodo);
+});
+
+module.exports = router;
